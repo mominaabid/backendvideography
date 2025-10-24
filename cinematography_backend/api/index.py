0a077711a17cda363a54741c4fdd605aaabb79b9
momina <technicmentors@gmail.com>
2025-10-25 00:47:32 +0500

    ready for depl

--- cinematography_backend/api/index.py
+++ cinematography_backend/api/index.py
@@ -1,5 +1,4 @@
 from vercel_python_wsgi import VercelWSGIHandler
-
 from cinematography_backend.wsgi import application
 
-handler = make_wsgi_handler(application)
+handler = VercelWSGIHandler(application)
