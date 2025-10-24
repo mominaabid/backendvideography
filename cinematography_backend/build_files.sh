37d8eb8b2a2ad88b0803500e2ba5a1d68dfcdad4
momina <technicmentors@gmail.com>
2025-10-25 00:30:01 +0500

    ready for deployyy4

--- cinematography_backend/build_files.sh
+++ cinematography_backend/build_files.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-# Install dependencies
 pip install -r requirements.txt
-
-# Collect static files for Django
 python3 manage.py collectstatic --noinput
-
-# Run migrations
 python3 manage.py migrate
+gunicorn cinematography_backend.wsgi
--- cinematography_backend/cinematography_backend/settings.py
+++ cinematography_backend/cinematography_backend/settings.py
@@ -7,7 +7,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get('SECRET_KEY', 'dev-secret-key')
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ['*']
+
 
 
 
