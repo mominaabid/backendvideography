85460c539db78e1bf11b3745415360aa79bd318c
momina <technicmentors@gmail.com>
2025-10-26 10:01:55 +0500

    cors

--- cinematography_backend/cinematography_backend/settings.py
+++ cinematography_backend/cinematography_backend/settings.py
@@ -119,12 +119,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS Settings
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:3000",
-    "http://localhost:8000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     "http://127.0.0.1:5173",
+#     "http://localhost:3000",
+#     "http://localhost:8000",
+# ]
+# CORS Settings
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 CORS_ALLOW_CREDENTIALS = True
 
