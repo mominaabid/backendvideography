935f3388f163b916c0f99c05439d8ce202f2c50e
momina <technicmentors@gmail.com>
2025-10-28 14:56:25 +0500

    deploy

--- cinematography_backend/cinematography_backend/settings.py
+++ cinematography_backend/cinematography_backend/settings.py
@@ -158,7 +158,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    
 }
 
 # -------------------------------------------------------------------
